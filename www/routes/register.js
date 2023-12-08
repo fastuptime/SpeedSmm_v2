7ba173dfe5daab3abbcdbaa96bfdd1ebf0d8736e
Can <63351166+fastuptime@users.noreply.github.com>
2023-12-08 21:39:19 +0300

    Update register.js

--- www/routes/register.js
+++ www/routes/register.js
@@ -117,6 +117,7 @@ module.exports = function (app, hesap) {
                 maxAge: 1000 * 60 * 60 * 24 * 5,
                 httpOnly: true,
             });
+            res.redirect("/login?message=Kayıt işlemi başarılı. Giriş yapabilirsiniz.");
         });
         
         
